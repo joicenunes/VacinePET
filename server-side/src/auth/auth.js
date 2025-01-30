be407d69bfb5ed825b1cfc6b3dec28ee0fa6c377
joicenunes <anunes.joice@gmail.com>
2025-01-29 21:20:22 -0300

    added authentication to route

--- server-side/src/auth/auth.js
+++ server-side/src/auth/auth.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import UserModel from '../models/userModel.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -7,7 +8,7 @@ export const authenticateUser = async (email, password) => {
   try {
     const errorMessage = 'Não foi possível realizar o login. Verifique suas credenciais e tente novamente.';
 
-    const user = await getUserByEmail(email);
+    const user = await UserModel.getUserByEmail(email);
 
     if (!user) {
       throw new Error(errorMessage);
@@ -24,5 +25,19 @@ export const authenticateUser = async (email, password) => {
     return { success: true, token };
   } catch (error) {
     return { error: error.message };
+  }
+};
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) return res.status(401).json({ error: 'Token não fornecido.' });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ error: 'Token inválido.' });
   }
 };
