90edfe8532e85d6c4ce0e9265b9bc25e9a6b98d7
joicenunes <anunes.joice@gmail.com>
2025-02-03 16:21:23 -0300

    removed authenticate from auth

--- server-side/src/auth/auth.js
+++ server-side/src/auth/auth.js
@@ -27,17 +27,3 @@ export const authenticateUser = async (email, password) => {
     return { error: error.message };
   }
 };
-
-export const authenticate = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-
-  if (!token) return res.status(401).json({ error: 'Token não fornecido.' });
-
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: 'Token inválido.' });
-  }
-};
