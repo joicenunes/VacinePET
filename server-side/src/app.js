df88eca434012923bf26707b897d966be76ef264
joicenunes <anunes.joice@gmail.com>
2025-02-03 20:43:03 -0300

    configured swagger

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import setupSwagger from './swagger/swagger.js';
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,5 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/pets', petRoutes);
+
+setupSwagger(app);
 
 export default app;
--- server-side/src/index.js
+++ server-side/src/index.js
@@ -3,4 +3,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
 });
