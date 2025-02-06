243be1f1430dbd7d906047c21eb2fd625e95b0fd
joicenunes <anunes.joice@gmail.com>
2025-02-06 20:42:12 -0300

    configure vaccines routes

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -4,6 +4,7 @@ import setupSwagger from './swagger/swagger.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import petRoutes from './routes/petRoutes.js';
+import vaccineRoutes from './routes/vaccineRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/pets', petRoutes);
+app.use('/api/vaccines', vaccineRoutes);
 
 setupSwagger(app);
 
